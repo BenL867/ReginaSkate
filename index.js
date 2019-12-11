43326cc24b6cbfd658e48dcfe05881e07fec5cfd
BenL867 <ben.lubiens@gmail.com>
2019-12-11 16:59:20 -0600

    controllers folder

--- index.js
+++ index.js
@@ -1,40 +1,35 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
-var mongoose = require('mongoose')
-var mongoDB = 'mongodb://127.0.0.1/my_database';
-mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
-var db = mongoose.connection;
+const mongoose = require('mongoose')
+const mongoDB = 'mongodb://127.0.0.1/my_database';
+const bodyParser = require("body-parser");
+const fileUpload = require("express-fileupload");
+const createPostController = require('./controllers/createPost')
+const homePageController = require('./controllers/homePage')
+const storePostController = require('./controllers/storePost')
+const getPostController = require('./controllers/getPost')
+const storePost = require('./middleware/storePost')
 
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => 'You are now connected to Mongo!')
+    .catch(err => console.error('Something went wrong', err))
 
 app.set('view engine', 'pug')
+app.use(fileUpload());
 app.use(express.static('public'));
+app.set('views', __dirname + '/views');
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+ 
 
-
-app.get('', (req,res) => {
-    res.render('index', {})
-})
-
-app.get('/', (req, res) => {
-  mongoClient.connect(url,  { useUnifiedTopology: true }, (err, client) => {
-   if(err) {
-       console.log('error')
-        return
-    }
-      const db = client.db('skate')
-       const collection = db.collection('userPosts')
-  })
-      collection.find({}).toarray((err, userPosts) => {    if(err) {
-          console.log('error')
-           return
-      }
-client.close()
-res.json(superheroes)
-      })
-
-})
+ 
+app.use('/posts/store', storePost)
+app.get("/", homePageController);
+app.get("/post/:id", getPostController);
+app.get("/posts/new", createPostController);
+app.post("/posts/store", storePostController);
 
 app.listen(port, () => {
     console.log(`Server running on http://localhost:${port}`);
---
+++ indexjs.md
@@ -0,0 +1,45 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+var mongoose = require('mongoose')
+var mongoDB = 'mongodb://127.0.0.1/my_database';
+mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => 'You are now connected to Mongo!')
+    .catch(err => console.error('Something went wrong', err))
+
+const bodyParser = require("body-parser");
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+app.set('view engine', 'pug')
+app.use(express.static('public'));
+
+
+
+app.get('', (req,res) => {
+    res.render('index', {})
+})
+
+app.get('/', (req, res) => {
+  mongoClient.connect(url,  { useUnifiedTopology: true }, (err, client) => {
+   if(err) {
+       console.log('error')
+        return
+    }
+      const db = client.db('skate')
+       const collection = db.collection('userPosts')
+  })
+      collection.find({}).toarray((err, userPosts) => {    if(err) {
+          console.log('error')
+           return
+      }
+client.close()
+res.json(userPosts)
+      })
+
+})
+
+app.listen(port, () => {
+    console.log(`Server running on http://localhost:${port}`);
+  })
